b47ef3be4d2a0aa11aa7067e3271d2012fdeb2cd
Vitkovskiy <Vikovskiy1912@mail.ru>
2026-02-04 14:55:13 +0300

    Create function ex 1 and 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let discriminant = Math.pow(b,2) - 4 * a * c;
+    if(discriminant == 0){
+        arr[0] = -b / (2 * a);
+    }
+    else if(discriminant > 0){
+        arr[0]=(- b + Math.sqrt(discriminant)) / (2 * a);
+        arr[1]=(- b - Math.sqrt(discriminant)) / (2 * a);
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let monthPercent = (percent / 100) / 12;
+    let creditBody = amount - contribution;
+    let monthPay = creditBody * (monthPercent + (monthPercent / (Math.pow((1 + monthPercent),countMonths) - 1)));
+    let summPay = monthPay * countMonths;
+    return parseFloat(summPay.toFixed(2));
 }
