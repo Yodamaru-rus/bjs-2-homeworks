8dedddfce135f21b26ed6db7ff83de646ba24b12
Vitkovskiy <Vikovskiy1912@mail.ru>
2026-02-08 14:11:24 +0300

    Create task func

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((element, index) => element === arr2[index])
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.age);
+  return result.length == 0
+    ? 0
+    : result.reduce((acc, item) => acc + item, 0) / result.length;
 }
